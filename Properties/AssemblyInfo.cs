15414508748ebbf59383dbda9a30f90c03bbf4c8
dkuznets <dkuznets@yandex.ru>
2016-10-19 09:12:41 +0300

    v 1.0.10.75 initial

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("OLO_CAN")]
+[assembly: AssemblyDescription("Программа настройки ОЛО через CAN")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("НПК СПП")]
+[assembly: AssemblyProduct("OLO_CAN")]
+[assembly: AssemblyCopyright("© DK'2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("12cce7bb-2d07-4a9f-a636-65b4f2c5eda4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.10.75")]
+[assembly: AssemblyFileVersion("1.0.10.75")]
