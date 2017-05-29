ea0609edc63e67fd0a98af0142c2a6ff6f977634
Denis Kuznetsov <dkuznets@yandex.ru>
2017-05-29 16:15:24 +0300

    20170529_161523

--- Form1.cs
+++ Form1.cs
@@ -2051,6 +2051,7 @@ namespace OLO_CAN
                     canmsg_t dat = new canmsg_t();
                     dat.data = new Byte[8];
                     pb_CMOS.Maximum = msg_count;
+/*
                     for (UInt32 i = 0; i < msg_count; i++)
                     {
 //                        canmsg_t dat = new canmsg_t();
@@ -2062,19 +2063,20 @@ namespace OLO_CAN
                             Trace.WriteLine("Err recv image data");
                             return;
                         }
-                        pb_CMOS.Value = (int)i;
-                        pb_CMOS.Refresh();
-                        pb_CMOS.Invalidate();
+//                        pb_CMOS.Value = (int)i;
+//                        pb_CMOS.Refresh();
+//                        pb_CMOS.Invalidate();
                         UInt32 data_size = dat.len;
                         for (UInt32 j = 0; j < data_size; j++)
                             image_data[j + image_data_count] = dat.data[j];
                         image_data_count += data_size;
                     }
-                    //if (uniCAN == null || !uniCAN.RecvPack(ref image_data, ref msg_count, 10000))
-                    //{
-                    //    Trace.WriteLine("Err recv image data");
-                    //    return;
-                    //}
+ */
+                    if (uniCAN == null || !uniCAN.RecvPack(ref image_data, ref msg_count, 10000))
+                    {
+                        Trace.WriteLine("Err recv image data");
+                        return;
+                    }
                     #region Режим калибровки (поиска плохих точек)
                     if (chb_Calibr.Checked)
                     {
