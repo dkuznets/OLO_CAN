58d0c567b7d6e5ab9c8cb5e4b6eb16644c8e9170
Denis Kuznetsov <dkuznets@yandex.ru>
2018-07-02 11:08:31 +0300

    20180702_110830

--- Form1.cs
+++ Form1.cs
@@ -6727,6 +6727,14 @@ namespace OLO_CAN
                 Trace.Write(" 0x" + msg.data[i].ToString("X2"));
             Trace.WriteLine("");
         }
+        void print2_msg(canmsg_t msg)
+        {
+            Trace.Write(" ID=" + ((rup_id.IDs)(msg.id - rup_id.RIGHT_WING_DEV_ID)).ToString() + " len=" + msg.len.ToString());
+            Trace.Write(" Data:");
+            for (int i = 0; i < msg.len; i++)
+                Trace.Write(" 0x" + msg.data[i].ToString("X2"));
+            Trace.WriteLine("");
+        }
         void print_msg(msg_t msg)
         {
             Trace.Write(" dID=" + msg.deviceID.ToString("X2") + " mID=" + msg.messageID.ToString("X2") + " len=" + msg.messageLen.ToString());
@@ -6967,7 +6975,7 @@ namespace OLO_CAN
                 Trace.WriteLine("Error recv STATUS_RESPONCE_ID");
                 return;
             }
-            print_msg(frame);
+            print2_msg(frame);
         }
         private void button14_Click(object sender, EventArgs e) //файлы
         {
@@ -6985,7 +6993,7 @@ namespace OLO_CAN
 
     public class rup_id
     {
-        public const UInt32 RECONFIG_ID                = (0x01 << 5);
+        public const UInt32 RECONFIG_ID = (0x01 << 5);
         public const UInt32 RUP_ID = (0x03 << 5);
         public const UInt32 STATUS_REQUEST_ID = (0x04 << 5);
         public const UInt32 STATUS_RESPONCE_ID = (0x05 << 5);
@@ -7002,5 +7010,27 @@ namespace OLO_CAN
         public const UInt32 FILE_TABLE_ADDRESS_ID = (0x3A << 5);
         public const UInt32 RIGHT_WING_DEV_ID = 0x11;
         public const UInt32 LEFT_WING_DEV_ID = 0x12;
+
+        public enum IDs : uint
+        {
+            RECONFIG_ID = (0x01 << 5),
+            RUP_ID = (0x03 << 5),
+            STATUS_REQUEST_ID = (0x04 << 5),
+            STATUS_RESPONCE_ID = (0x05 << 5),
+            ACTIV_FLASH_ID = (0x30 << 5),
+            WRITE_DATA_ID = (0x31 << 5),
+            READ_DATA_ID = (0x32 << 5),
+            ACK_ID = (0x33 << 5),
+            DATA_SEGMENT_ID = (0x34 << 5),
+            ERASE_ID = (0x35 << 5),
+            AREA_ERASE_REQUEST_ID = (0x36 << 5),
+            FILE_TABLE_REQUEST_ID = (0x37 << 5),
+            FLASH_TABLE_RESPONCE_ID = (0x38 << 5),
+            AREA_ERASE_RESPONCE_ID = (0x39 << 5),
+            FILE_TABLE_ADDRESS_ID = (0x3A << 5),
+            RIGHT_WING_DEV_ID = 0x11,
+            LEFT_WING_DEV_ID = 0x12
+        };
+
     }
 }
