0b7332df8f96f40d3ab2990c52b48b091046f52e
Denis Kuznetsov <dkuznets@yandex.ru>
2018-07-05 15:02:20 +0300

    20180705_150220

--- Form1.cs
+++ Form1.cs
@@ -7392,7 +7392,7 @@ namespace OLO_CAN
                     listBox1.Items.Insert(0, "Error send AREA_ERASE_REQUEST_ID");
                     return;
                 }
-                if (uniCAN == null || !uniCAN.Recv(ref frame, 10000))
+                if (uniCAN == null || !uniCAN.Recv(ref frame, 1000))
                 {
                     listBox1.Items.Insert(0, "Error recv AREA_ERASE_RESPONCE_ID");
                     return;
