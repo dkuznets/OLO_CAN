17b8a207d4ee728ed52df7935e910c33695f00a6
Denis Kuznetsov <dkuznets@yandex.ru>
2018-07-05 15:03:51 +0300

    20180705_150351

--- Form1.cs
+++ Form1.cs
@@ -7416,12 +7416,6 @@ namespace OLO_CAN
                     listBox1.Items.Insert(0, "Error send ERASE_ID");
                     return;
                 }
-                if (uniCAN == null || !uniCAN.Recv(ref frame, 10000))
-                {
-                    listBox1.Items.Insert(0, "Error recv AREA_ERASE_RESPONCE_ID");
-                    return;
-                }
-                msg_2_log(frame);
 
                 progressBar1.Value = 0;
                 progressBar1.Maximum = 15;
