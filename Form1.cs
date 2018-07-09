e66feb7b6b0715cf2abab8b756605a528180902d
Denis Kuznetsov <dkuznets@yandex.ru>
2018-07-09 16:13:48 +0300

    20180709_161348

--- Form1.cs
+++ Form1.cs
@@ -7981,7 +7981,7 @@ namespace OLO_CAN
 
         private void button3_Click_1(object sender, EventArgs e)
         {
-            UInt32 x0 = 123456, x1 = 123457, x2 = 123457, xx = 0;;
+            UInt32 x0 = 123456, x1 = 123457, x2 = 123458, xx = 0;;
             xx = (x0 & x1) | (x0 & x2) | (x1 & x2);
             MessageBox.Show(xx.ToString());
         }
