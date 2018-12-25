a3d59009baac8914e64df61250a763bc02eb2fab
Denis Kuznetsov <dkuznets@yandex.ru>
2018-12-25 09:32:14 +0300

    20181225_093213

--- Form1.cs
+++ Form1.cs
@@ -2245,9 +2245,8 @@ namespace OLO_CAN
 
             // Читаем температуру CMOS1
             pb_CMOS.Value = 25;
-            Application.DoEvents();
-//            pb_CMOS.Refresh();
-//            Thread.Sleep(50);
+            pb_CMOS.Refresh();
+            Thread.Sleep(100);
             cmd.magic = Const.MAGIC_BYTE;
             cmd.cmd = Const.COMMAND_CMOS1_GET_TEMPERATURE;
 
@@ -2266,9 +2265,8 @@ namespace OLO_CAN
             cmd.cmd = Const.COMMAND_CMOS2_GET_TEMPERATURE;
 
             pb_CMOS.Value = 50;
-            Application.DoEvents();
-            //            pb_CMOS.Refresh();
-            //            Thread.Sleep(50);
+            pb_CMOS.Refresh();
+            Thread.Sleep(100);
             if (!SendCommand(cmd, ref res))
             {
                 chb_PRunVideo.CheckState = CheckState.Unchecked;
@@ -2282,9 +2280,8 @@ namespace OLO_CAN
             lb_T2_val.Text = fT2.ToString("'+'0.0'°';'-'0.0'°';'0.0°'");
 
             pb_CMOS.Value = 75;
-            Application.DoEvents();
-            //            pb_CMOS.Refresh();
-            //            Thread.Sleep(50);
+            pb_CMOS.Refresh();
+            Thread.Sleep(100);
             UInt32 shot_pixels = 0;
 
             // Чтение картинки
@@ -2295,14 +2292,11 @@ namespace OLO_CAN
             if (_state != State.VideoState)
                 return;
 
-            Application.DoEvents();
+            pb_CMOS.Value = 100;
+            pb_CMOS.Refresh();
             Thread.Sleep(100);
             if (SendCommand(cmd, ref res) || res.stat == Const.STATUS_OK)     
             {
-                pb_CMOS.Value = 100;
-                Application.DoEvents();
-                //            pb_CMOS.Refresh();
-                //            Thread.Sleep(50);
                 pb_CMOS.Value = 0;
                 if (!chb_PFIFO.Checked)
                 {
