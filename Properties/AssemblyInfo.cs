f07125673d45b22969c564d97a7fb82f56409025
Denis Kuznetsov <dkuznets@yandex.ru>
2019-01-17 10:00:19 +0300

    20190117_100018

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.8.253")]
-[assembly: AssemblyFileVersion("1.4.8.253")]
+[assembly: AssemblyVersion("1.5.0.264")]
+[assembly: AssemblyFileVersion("1.5.0.264")]
